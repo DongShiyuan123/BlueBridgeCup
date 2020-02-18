7106d42ba28dd4e068a81d93db07cf5a1d894222
Dong <807606445@qq.com>
2020-02-18 18:11:22 +0800

    第一次提交

--- src/main/java/org/lanqiao/algo/elementary/_01xor/_03_1的个数.java
+++ src/main/java/org/lanqiao/algo/elementary/_01xor/_03_1的个数.java
@@ -2,33 +2,41 @@ package org.lanqiao.algo.elementary._01xor;
 
 import java.util.Scanner;
 
+/**
+ * 请实现一个函数，输入一个整数，输出二进制中1的个数
+ * 例：9的二进制表示为1001，有2位是1
+ * <p>
+ * 判断一个整数是不是2的整数次方（二进制一条语句解决 if (((N - 1) & N) == 0)  ）
+ */
 public class _03_1的个数 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    System.out.println(Integer.toString(N, 2));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(Integer.toString(N, 2));
 
-    int count = 0;
-    //比对每一位
-    for (int i = 0; i < 32; i++) {
-      if ((N & (1 << i)) == (1 << i)) {
-        count++;
-      }
-    }
-    System.out.println(count);
+        int count = 0;
+        //1
+        //比对每一位
+        for (int i = 0; i < 32; i++) {
+            if ((N & (1 << i)) == (1 << i)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        //2
+        count = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((N >>> i) & 1) == 1)
+                count++;
+        }
+        System.out.println(count);
+        //3
+        count = 0;
+        while (N != 0) {
+            N = ((N - 1) & N);
+            count++;
+        }
+        System.out.println(count);
 
-    count = 0;
-    for (int i = 0; i < 32; i++) {
-      if (((N >>> i) & 1) == 1)
-        count++;
     }
-    System.out.println(count);
-
-    count = 0;
-    while (N != 0) {
-      N = ((N - 1) & N);
-      count++;
-    }
-    System.out.println(count);
-  }
 }
