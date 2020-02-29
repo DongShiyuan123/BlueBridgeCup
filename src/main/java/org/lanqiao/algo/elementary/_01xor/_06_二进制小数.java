2aae6c54c706814e74015e18c769f25d2de253a5
Dong <807606445@qq.com>
2020-03-01 05:26:25 +0800

    第一次提交

--- src/main/java/org/lanqiao/algo/elementary/_01xor/_06_二进制小数.java
+++ src/main/java/org/lanqiao/algo/elementary/_01xor/_06_二进制小数.java
@@ -1,28 +1,33 @@
 package org.lanqiao.algo.elementary._01xor;
 
+/*
+ * 给定一个介于0和1之间的实数，如（0.625），类型为double打印他的二进制表示（0.101，小数点后的二进制分别表示0.5,0.25,0.125 ....)
+ * 如果该数字无法精确的用32位以内的二进制表示，则打印“ERROR”
+ *
+ * */
 public class _06_二进制小数 {
-  public static void main(String[] args) {
-    double num = 0.625;
-    StringBuilder sb = new StringBuilder("0.");
-    while (num > 0) {
-      //乘2：挪整
-      double r = num * 2;
-      //判断整数部分
-      if (r >= 1) {
-        sb.append("1");
-        //消掉整数部分
-        num = r - 1;
-      } else {
-        sb.append("0");
-        num = r;
-      }
+    public static void main(String[] args) {
+        double num = 0.625;
+        StringBuilder sb = new StringBuilder("0.");
+        while (num > 0) {
+            //乘2：挪整
+            double r = num * 2;
+            //判断整数部分
+            if (r >= 1) {
+                sb.append("1");
+                //消掉整数部分
+                num = r - 1;
+            } else {
+                sb.append("0");
+                num = r;
+            }
 
-      if (sb.length() > 34) {
-        System.out.println("ERROR");
-        return;
-      }
+            if (sb.length() > 34) {
+                System.out.println("ERROR");
+                return;
+            }
 
+        }
+        System.out.println(sb.toString());
     }
-    System.out.println(sb.toString());
-  }
 }
--- src/main/java/org/lanqiao/algo/elementary/_01xor/_07_出现K次.java
+++ src/main/java/org/lanqiao/algo/elementary/_01xor/_07_出现K次.java
@@ -1,37 +1,43 @@
 package org.lanqiao.algo.elementary._01xor;
 
+/*
+ *数组中只有一个数出现1次，其他的数都出现了k次，请输出只出现了1次的数
+ *
+ * k个k进制数，不进位加法等于0（三个三进制数不进位相加等于0,10个十进制分数不进位相加等于0）
+ * */
 public class _07_出现K次 {
-  public static void main(String[] args) {
-    int[] arr = {2, 2, 2, 9, 7, 7, 7, 3, 3, 3, 6, 6, 6, 0, 0, 0};
-    int len = arr.length;
-    char[][] kRadix = new char[len][];
-    int k = 3;
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 2, 9, 7, 7, 7, 3, 3, 3, 6, 6, 6, 0, 0, 0};
+        int len = arr.length;
+        //三进制字符数组
+        char[][] kRadix = new char[len][];
+        int k = 3;
 
-    int maxLen = 0;
-    //转成k进制字符数组
-    //对于每个数字
-    for (int i = 0; i < len; i++) {
-      //求每个数字的三进制字符串并翻转，然后转为字符数组
-      kRadix[i] = new StringBuilder(Integer.toString(arr[i], k)).reverse().toString().toCharArray();
-      if (kRadix[i].length > maxLen)
-        maxLen = kRadix[i].length;
-    }
-    //不进位加法
-    int[] resArr = new int[maxLen];
-    for (int i = 0; i < len; i++) {
-      //  不进位加法
-      for (int j = 0; j < maxLen; j++) {
-        if (j >= kRadix[i].length)
-          resArr[j] += 0;
-        else
-          resArr[j] += (kRadix[i][j] - '0');
-      }
-    }
+        int maxLen = 0;
+        //转成k进制字符数组
+        //对于每个数字
+        for (int i = 0; i < len; i++) {
+            //求每个数字的三进制字符串并翻转，然后转为字符数组
+            kRadix[i] = new StringBuilder(Integer.toString(arr[i], k)).reverse().toString().toCharArray();
+            if (kRadix[i].length > maxLen)
+                maxLen = kRadix[i].length;
+        }
+        //不进位加法
+        int[] resArr = new int[maxLen];
+        for (int i = 0; i < len; i++) {
+            //  不进位加法
+            for (int j = 0; j < maxLen; j++) {
+                if (j >= kRadix[i].length)
+                    resArr[j] += 0;
+                else
+                    resArr[j] += (kRadix[i][j] - '0');
+            }
+        }
 
-    int res = 0;
-    for (int i = 0; i < maxLen; i++) {
-      res += (resArr[i] % k) * (int) (Math.pow(k, i));// 8%3=2,
+        int res = 0;
+        for (int i = 0; i < maxLen; i++) {
+            res += (resArr[i] % k) * (int) (Math.pow(k, i));// 8%3=2,
+        }
+        System.out.println(res);
     }
-    System.out.println(res);
-  }
 }
