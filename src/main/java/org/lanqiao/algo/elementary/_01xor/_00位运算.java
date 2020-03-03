93161ab68b5926e9d990076e2045e6e770be8ca6
Dong <807606445@qq.com>
2020-03-03 17:53:51 +0800

    第一次提交

--- src/main/java/org/lanqiao/algo/elementary/_01xor/_00位运算.java
+++ src/main/java/org/lanqiao/algo/elementary/_01xor/_00位运算.java
@@ -1,8 +1,8 @@
 package org.lanqiao.algo.elementary._01xor;
 
 public class _00位运算 {
-  public static void main(String[] args) {
-    System.out.println((1 << 35));
-    System.out.println((1 << 3));
-  }
+    public static void main(String[] args) {
+        System.out.println((1 << 35));
+        System.out.println((1 << 3));
+    }
 }
