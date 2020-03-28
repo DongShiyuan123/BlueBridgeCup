3715d54988a1c48fee7ccdd68674f1937610c35d
Dong <807606445@qq.com>
2020-03-28 22:02:40 +0800

    第一次提交

--- src/main/java/org/lanqiao/algo/elementary/_02searchAndSort/Case02_旋转数组的最小数字.java
+++ src/main/java/org/lanqiao/algo/elementary/_02searchAndSort/Case02_旋转数组的最小数字.java
@@ -7,7 +7,7 @@ import org.assertj.core.api.Assertions;
  * 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，
  * 该数组的最小值为1.
  * <p>
- * 考点：活用二分查找
+ * 考点：活用二分查找（最小的数字在无序的一侧）
  */
 public class Case02_旋转数组的最小数字 {
     static int min(int[] arr) {
